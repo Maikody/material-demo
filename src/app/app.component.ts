e9b3962e1c87e3a3f6b06b22e70fbf4f9c73162d
Maikody <73773432+Maikody@users.noreply.github.com>
2021-07-03 17:32:54 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { timer } from 'rxjs';
+import { EditCourseComponent } from './edit-course/edit-course.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'material-demo';
+  // progress = 0;
+  // timer;
+  courses;
+  isLoading = false;
+
+  constructor(private dialog: MatDialog) {
+    // this.timer = setInterval(() => {
+    //     this.progress++;
+    //     if (this.progress == 100) {
+    //       clearInterval(this.timer);
+    //     }
+    // }, 20);
+    
+    this.isLoading = true;
+    this.getCourses().subscribe(x => this.isLoading = false);
+  }
+
+  getCourses() {
+    return timer(2000);
+  }
+
+  isChecked = true;
+
+  colors = [
+    {id:1, name: "blue"},
+    {id:2, name: "yellow"},
+  ];
+
+  categories = [
+    {name: "Begginer"},
+    {name: "Intermediate"},
+    {name: "Advanced"}
+  ];
+
+  selectCategory(category) {
+    this.categories
+        .filter(c => c != category)
+        .forEach(c => c['selected'] = false);
+
+    category.selected = !category.selected;
+  }
+
+  onChange($event) {
+    console.log($event);
+  }
+
+  openDialog() {
+    this.dialog.open(EditCourseComponent, {
+      data: {courseId: 1}
+    })
+      .afterClosed()
+      .subscribe(result => console.log(result));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditCourseComponent } from './edit-course/edit-course.component';
+import { CourseService } from './course.service';
+import { MatComponentsModule } from './mat-components.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditCourseComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatComponentsModule
   ],
-  providers: [],
+  providers: [
+    CourseService,
+    // {provide: DIALOG_DATA, useValue:{}}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
